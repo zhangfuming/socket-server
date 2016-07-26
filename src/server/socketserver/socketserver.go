dfd06e06ea5b1412ff7a9cf275cd4355cdcae955
张福明 <zhangfuming@zhangfumingdeMacBook-Pro.local>
2016-07-26 19:49:22 +0800

    添加心跳代码

--- src/server/socketserver/socketserver.go
+++ src/server/socketserver/socketserver.go
@@ -6,7 +6,10 @@ import (
 	"net"
 	"os"
 	"server/protocol"
+	"time"
 )
+
+var HEART_MSG []byte = []byte("0")
 
 var sessions map[string]net.Conn = make(map[string]net.Conn)
 
@@ -18,6 +21,7 @@ func StartSocket(servernetwork string, serveraddress string, flag chan bool) {
 		flag <- true
 	}()
 	Log("socket server start success on ", serveraddress, servernetwork)
+	go scanSession()
 	for {
 		conn, err := netListen.Accept()
 		if err != nil {
@@ -39,6 +43,7 @@ func addSession(conn net.Conn) {
 func removeSession(conn net.Conn) {
 	for key, val := range sessions {
 		if val == conn {
+			val.Close()
 			delete(sessions, key)
 		}
 	}
@@ -50,19 +55,19 @@ func Broadcast(msg []byte) {
 	}
 }
 
-func senderToClient(conn net.Conn, msg []byte) {
-	conn.Write(protocol.Packet(msg))
+func senderToClient(conn net.Conn, msg []byte)(count int,error error) {
+	return conn.Write(protocol.Packet(msg))
 }
 
 func handleConnection(conn net.Conn) {
 	tmpBuffer := make([]byte, 0)
 	readerChannel := make(chan []byte, 16)
-	go reader(readerChannel)
+	go reader(conn,readerChannel)
 	buffer := make([]byte, 1024)
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
-			Log(conn.RemoteAddr().String(), "client connect error!", err)
+			Log(conn.RemoteAddr().String(), "client connect error, remove client session!", err)
 			removeSession(conn)
 			return
 		}
@@ -70,12 +75,29 @@ func handleConnection(conn net.Conn) {
 	}
 }
 
-func reader(readerChannel chan []byte) {
+func reader(conn net.Conn,readerChannel chan []byte) {
 	for {
 		select {
 		case data := <-readerChannel:
 			Log(string(data))
+			if string(data) == string(HEART_MSG){
+				//发送心跳回应
+				senderToClient(conn,HEART_MSG)
+			}
 		}
+	}
+}
+
+func scanSession(){
+	for{
+		for _, val := range sessions {
+			_,err := senderToClient(val, HEART_MSG)
+			if err != nil{
+				Log("send heart to client failed, close session",err)
+				removeSession(val)
+			}
+		}
+		time.Sleep(5 * time.Second)
 	}
 }
 
--- test/client.go
+++ test/client.go
@@ -1,6 +1,6 @@
 package main
 import (
-	"../src/protocol"
+	"server/protocol"
 	"fmt"
 	"net"
 	"os"
